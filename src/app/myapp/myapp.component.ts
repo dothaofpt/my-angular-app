fe573ea6b8b61c8c7b9c7adba2db0acadd743eed
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-10-21 14:04:19 +0700

    up

---
+++ src/app/myapp/myapp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myapp',
+  standalone: true,
+  imports: [],
+  templateUrl: './myapp.component.html',
+  styleUrl: './myapp.component.css'
+})
+export class MyappComponent {
+
+}
